4749c8e88c6f6056e087b739f55ffcaffe387501
VaniaHutman <vaniahutman@gmail.com>
2021-03-28 18:30:38 +0300

    connected to database

--- TasksPage.xaml.cs
+++ TasksPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
 
 namespace PDA2._0_
 {
@@ -22,6 +23,43 @@ namespace PDA2._0_
         {
             InitializeComponent();
             openTask.Content = new OpenTask();
+            ReadDB();
+        }
+
+        public void ReadDB()
+        {
+            List<Task> listOfTasks = new List<Task>();
+            string sqlExpression = "SELECT * FROM Tasks";
+            using (var connection = new SqliteConnection("Data Source=database.db"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            var id = reader.GetValue(0);
+                            var name = reader.GetValue(1);
+                            var date = reader.GetValue(2);
+                            var text = reader.GetValue(3);
+                            Label nameLabel = new Label();
+                            nameLabel.Content = name.ToString();
+                            nameLabel.Foreground = new SolidColorBrush(Colors.White);
+                            Label idLabel = new Label();
+                            idLabel.Content = id.ToString();
+                            idLabel.Foreground = new SolidColorBrush(Colors.White);
+                            StackPanel stackPanel = new StackPanel();
+                            stackPanel.Children.Add(idLabel);
+                            stackPanel.Children.Add(nameLabel);
+                            stackPanel.Orientation = Orientation.Horizontal;
+                            tasksList.Items.Add(stackPanel);
+                        }
+                    }
+                }
+            }
         }
     }
 }
